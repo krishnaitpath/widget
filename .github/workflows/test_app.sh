4011c2118afb280ffbe18e8828713b394b4c3e34
krishnaitpath <122959850+krishnaitpath@users.noreply.github.com>
2023-01-18 18:16:05 +0530

    Update test_app.sh

--- .github/workflows/test_app.sh
+++ .github/workflows/test_app.sh
@@ -2,7 +2,8 @@
 
 set -eo pipefail
 
+
 xcodebuild -workspace widget.xcworkspace \
             -scheme widget\ iOS \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
-            clean test | xcpretty
+            clean test | xcpretty 
